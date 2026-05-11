0911bb1c81fc632629120638417af4db994814d7
canzonadzka <canzonadzka@gmail.com>
2026-05-11 10:24:29 +0700

    menambah destructor

--- agregasianak.cpp
+++ agregasianak.cpp
@@ -7,3 +7,8 @@ string nama;
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
