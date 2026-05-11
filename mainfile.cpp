90c3fcf29bbcd3a77b773b918c85a0ca27629926
canzonadzka <canzonadzka@gmail.com>
2026-05-11 10:40:46 +0700

    alokasi objek anak

--- mainfile.cpp
+++ mainfile.cpp
@@ -7,3 +7,6 @@ using namespace std;
 int main() { //agregasi
     ibu* varIbu = new ibu("dini");
     ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
